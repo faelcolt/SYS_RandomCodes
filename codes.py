85fe562fa9cadd2d21c8d159c47c84d4119bec73
Rafael Cordeiro <fael_colt@hotmail.com>
2023-03-07 20:29:21 -0300

    att

--- codes.py
+++ codes.py
@@ -1,7 +1,13 @@
 listaDeNomes = list()
+contador = 1
 
 while True:
-    nome = str(input('Nome: '))
+    nome = str(input(f'Digite o {contador}º nome: ')).capitalize()
     listaDeNomes.append(nome)
+    continuar = str(input('Deseja continuar? [S/N] ')).upper().strip()
+    contador += 1
+    if continuar == 'N':
+        break
     
+print(f'Os nomes digitados foram {listaDeNomes}')
     
