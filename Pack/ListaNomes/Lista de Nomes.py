dcb676a82888c6eae0fab8b66f78f0cad2b0f09b
Rafael Cordeiro <fael_colt@hotmail.com>
2023-03-08 17:32:50 -0300

    att

--- Pack/ListaNomes/Lista de Nomes.py
+++ Pack/ListaNomes/Lista de Nomes.py
@@ -1,36 +1,45 @@
 ''' Função para criar uma lista de nomes e listá-los na ordem em que foram digitados com separação semântica e depois criar
 um documento em word para ser reutilizado. ''' 
 
+
 # adiciona os nomes digitados em uma lista
-listaDeNomes = list()
+lista_de_nomes = list()
 contador = 1
+
 
 while True:
     nome = str(input(f'Digite o {contador}º nome: ')).title()
-    listaDeNomes.append(nome)
+    lista_de_nomes.append(nome)
     continuar = str(input('Deseja continuar? [S/N] ')).upper().strip()
     contador += 1
     if continuar == 'N':
         break
 
+
 # separa os nomes digitados de forma semântica
 posicaoNome = 0
 print('Os nomes digitados foram: ', end='')
-for c in range(0, len(listaDeNomes)):
-    if listaDeNomes[posicaoNome] == listaDeNomes[-1] and len(listaDeNomes) != 1:
+for c in range(0, len(lista_de_nomes)):
+    if lista_de_nomes[posicaoNome] == lista_de_nomes[-1] and len(lista_de_nomes) != 1:
         print('e ', end='')
-    print(f'{listaDeNomes[posicaoNome]}', end='')
-    if listaDeNomes[posicaoNome] == listaDeNomes[-1]:
+    print(f'{lista_de_nomes[posicaoNome]}', end='')
+    if lista_de_nomes[posicaoNome] == lista_de_nomes[-1]:
         print('. ', end='')
-    elif listaDeNomes[posicaoNome] == listaDeNomes[-2]:
+    elif lista_de_nomes[posicaoNome] == lista_de_nomes[-2]:
         print(' ', end='')
     else:
         print(', ', end='')
     posicaoNome += 1
+print()
+
+
+# essa parte do código permite que você dê um nome para o arquivo antes de criá-lo
+nome_do_arquivo = str(input('Digite o nome do arquivo: '))
+
 
 # essa parte do código cria um documento em word para ser utilizado separadamente do código
-with open('lista_de_nomes.txt', 'w') as arquivo:
-    for texto in listaDeNomes:
+with open(f'{nome_do_arquivo}.txt', 'w') as arquivo:
+    for texto in lista_de_nomes:
         arquivo.write(str(texto) + '\n')
 
 # em andamento...
