86bc4913bc124b4f83c2f12b76c28fa6eb054e3a
Rafael Cordeiro <fael_colt@hotmail.com>
2023-03-08 07:15:16 -0300

    att

--- Pack/listaDeNomes.py
+++ Pack/listaDeNomes.py
@@ -1,4 +1,4 @@
-''' Função para criar uma lista de nomes e listá-los na ordem em que foram digitados, com separação semântica e depois criar
+''' Função para criar uma lista de nomes e listá-los na ordem em que foram digitados com separação semântica e depois criar
 um documento em word para ser reutilizado. ''' 
 
 # adiciona os nomes digitados em uma lista
