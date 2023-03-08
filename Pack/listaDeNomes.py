63d0fd51d291d7cec1ea88911db8860ccc17900e
Rafael Cordeiro <fael_colt@hotmail.com>
2023-03-07 21:26:32 -0300

    att

--- Pack/listaDeNomes.py
+++ Pack/listaDeNomes.py
@@ -1,19 +1,19 @@
 '''função para criar uma lista de nomes e listá-los na ordem em que foram digitados, com separação semântica.''' 
 
-# essa parte do código adiciona os nomes digitados em uma lista
+# adiciona os nomes digitados em uma lista
 def listaDeNomes():
     listaDeNomes = list()
     contador = 1
 
     while True:
-        nome = str(input(f'Digite o {contador}º nome: ')).capitalize()
+        nome = str(input(f'Digite o {contador}º nome: ')).title()
         listaDeNomes.append(nome)
         continuar = str(input('Deseja continuar? [S/N] ')).upper().strip()
         contador += 1
         if continuar == 'N':
             break
 
-# essa parte do código separa os nomes digitados de forma semântica e coerente
+# separa os nomes digitados de forma semântica
     posicaoNome = 0
     print('Os nomes digitados foram: ',end='')
     for c in range(0, len(listaDeNomes)):
