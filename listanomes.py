5d38119fa2a7e8e4361364156b04379875ec7bba
Rafael Cordeiro <fael_colt@hotmail.com>
2023-03-11 07:31:48 -0300

    att

--- listanomes.py
+++ listanomes.py
@@ -1,5 +1,5 @@
-''' Função para criar uma lista de nomes e listá-los na ordem em que foram digitados com separação semântica e depois criar
-um documento em word para ser reutilizado. ''' 
+""" Função para criar uma lista de nomes e listá-los na ordem em que foram digitados com separação semântica e depois criar
+um documento em word para ser reutilizado. """
 
 
 def listando_nomes():
@@ -7,13 +7,12 @@ def listando_nomes():
     lista_de_nomes = list()
     contador = 1
     while True:
-        nome = str(input(f'Digite o {contador}º nome: ')).title()
+        nome = str(input(f"Digite o {contador}º nome: ")).title()
         lista_de_nomes.append(nome)
         continuar = str(input('Deseja continuar? [S/N] ')).upper().strip()
         contador += 1
         if continuar == 'N':
             break
-
 
     # separa os nomes digitados na lista de forma semântica
     posicaoNome = 0
@@ -31,15 +30,12 @@ def listando_nomes():
         posicaoNome += 1
     print()
 
-
     # permite que você dê um nome para o arquivo antes de criá-lo
     nome_do_arquivo = str(input('Digite o nome do arquivo: '))
 
-
     # essa função permite que a pessoa escolha se quer ler, escrever, modificar, apagar ou juntar arquivos
     # em andamento...
-    
-    
+
     # cria um documento em word para ser utilizado separadamente do código
     with open(f'{nome_do_arquivo}.txt', 'w') as arquivo:
         for texto in lista_de_nomes:
